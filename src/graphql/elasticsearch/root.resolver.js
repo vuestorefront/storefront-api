8127b2c1f34495c7f8f12f154405623c88406bf8
pkarw <pkarwatka@divante.pl>
2019-11-26 12:11:52 +0100

    eslint fixes

--- src/graphql/elasticsearch/root.resolver.js
+++ src/graphql/elasticsearch/root.resolver.js
@@ -4,10 +4,10 @@ const resolver = {
     version: (_, { search }, context, rootValue) => { return process.env.npm_package_version } // entry point for product extensions
   },
   ESResponseInterface: {
-    __resolveType() {
-        return null;
-      }      
-  }  
+    __resolveType () {
+      return null;
+    }
+  }
 };
 
 export default resolver;
