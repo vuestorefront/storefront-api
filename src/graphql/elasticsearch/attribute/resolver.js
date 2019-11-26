d33d5f0b67d4ffe16c685d3dd4b2b574ae35db1c
pkarw <pkarwatka@divante.pl>
2019-11-26 12:09:35 +0100

    Attrbite resolver backward compatibility

--- src/graphql/elasticsearch/attribute/resolver.js
+++ src/graphql/elasticsearch/attribute/resolver.js
@@ -3,18 +3,23 @@ import client from '../client';
 import { buildQuery } from '../queryBuilder';
 import { getIndexName } from '../mapping'
 
-async function listAttributes (attributes, context, rootValue, _sourceInclude) {
+async function listAttributes (attributes, context, rootValue, _sourceInclude, _sourceExclude) {
   let query = buildQuery({ filter: attributes, pageSize: 150, type: 'attribute' });
-
-  if (_sourceInclude === undefined) {
-    _sourceInclude = config.entities.attribute.includeFields
-  }
 
   const response = await client.search({
     index: getIndexName(context.req.url),
     type: config.elasticsearch.indexTypes[3],
     body: query,
-    _sourceInclude
+    _source_include: _sourceInclude,
+    _source_exclude: _sourceExclude
+  });
+
+  response.items = []
+  response.total_count = response.hits.total
+  response.hits.hits.forEach(hit => {
+    let item = hit._source
+    item._score = hit._score
+    response.items.push(item)
   });
 
   return response;
@@ -22,7 +27,7 @@ async function listAttributes (attributes, context, rootValue, _sourceInclude) {
 
 const resolver = {
   Query: {
-    customAttributeMetadata: (_, { attributes, _sourceInclude }, context, rootValue) => listAttributes(attributes, context, rootValue, _sourceInclude)
+    customAttributeMetadata: (_, { attributes, _sourceInclude, _sourceExclude }, context, rootValue) => listAttributes(attributes, context, rootValue, _sourceInclude, _sourceExclude)
   }
 };
 
