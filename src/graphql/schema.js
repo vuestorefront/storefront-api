0bc5e39efe0b21931af5a6d7453e833d976c6645
pkarw <pkarwatka@divante.pl>
2019-11-26 11:17:31 +0100

    Playground support added, root schemas added

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,12 +2,13 @@ import path from 'path';
 import config from 'config';
 import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
 
+const rootSchemas = fileLoader(path.join(__dirname, `./${config.server.searchEngine}/*.graphqls`))
 const coreSchemas = fileLoader(
   path.join(__dirname, `./${config.server.searchEngine}/**/*.graphqls`)
 );
 const extensionsSchemas = fileLoader(
   path.join(__dirname, `../api/extensions/**/*.graphqls`)
 );
-const typesArray = coreSchemas.concat(extensionsSchemas)
+const typesArray = rootSchemas.concat(coreSchemas).concat(extensionsSchemas)
 
 export default mergeTypes(typesArray, { all: true });
