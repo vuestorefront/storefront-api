046f3362c9ef71efed8365f52d119b37bb30d423
ResuBaka <resubaka@gmail.com>
2020-01-26 12:01:06 +0100

    fix(lib): fixed if to only load TagCache instance when active

--- packages/lib/cache-instance.ts
+++ packages/lib/cache-instance.ts
@@ -2,7 +2,7 @@ import TagCache from 'redis-tag-cache'
 import config from 'config'
 let cache: boolean|TagCache = false
 
-if (config.has('server.useOutputCache')) {
+if (config.has('server.useOutputCache') && config.get('server.useOutputCache')) {
   const redisConfig = Object.assign(config.get('redis'))
   cache = new TagCache({
     redis: redisConfig,
