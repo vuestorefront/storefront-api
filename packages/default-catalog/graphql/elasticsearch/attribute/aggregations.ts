e3d63a60f0cf42bfa4f20939aa87839bfef6566b
pkarw <pkarwatka@divante.pl>
2020-04-07 18:08:29 +0200

    Update aggregations.ts

--- packages/default-catalog/graphql/elasticsearch/attribute/aggregations.ts
+++ packages/default-catalog/graphql/elasticsearch/attribute/aggregations.ts
@@ -1,19 +1,19 @@
 import AttributeService from '../../../api/attribute/service'
 import { IConfig } from 'config'
 
-export async function aggregationsToAttributes(response: { aggregations: any, attribute_metadata: Array<{
-    is_visible_on_front: any,
-    is_visible: any,
-    default_frontend_label: any,
-    attribute_id: any,
-    entity_type_id: any,
-    id: any,
-    is_user_defined: any,
-    is_comparable: any,
-    attribute_code: any,
-    slug: any,
-    options: any
-  }>
+export async function aggregationsToAttributes (response: { aggregations: any, attribute_metadata: {
+  is_visible_on_front: any,
+  is_visible: any,
+  default_frontend_label: any,
+  attribute_id: any,
+  entity_type_id: any,
+  id: any,
+  is_user_defined: any,
+  is_comparable: any,
+  attribute_code: any,
+  slug: any,
+  options: any
+}[]
 }, config: IConfig, esIndex: string) {
   if (response.aggregations && config.get('entities.attribute.loadByAttributeMetadata')) {
     const attributeListParam = AttributeService.transformAggsToAttributeListParam(response.aggregations)
